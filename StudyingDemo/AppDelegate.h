16a3ee7d2077c45194a50424cffd272e931303a3
tinreslutely <tinreslutely@hotmail.com>
2015-12-16 14:46:23 +0800

    initiate project
    Project is created on  Xcode 7.1

---
+++ StudyingDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudyingDemo
+//
+//  Created by 李晓毅 on 15/12/16.
+//  Copyright © 2015年 李晓毅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StudyingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StudyingDemo
+//
+//  Created by 李晓毅 on 15/12/16.
+//  Copyright © 2015年 李晓毅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
